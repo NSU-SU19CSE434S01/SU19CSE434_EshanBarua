22267222b01389fdfeadf85904b7252c019044de
BaruaEshan <eshan.barua@northsouth.edu>
2019-07-16 00:04:08 +0600

    Setting title & exit button event handler on JFX scene

--- Project01/JavaFXApplication2/src/javafxapplication2/JavaFXApplication2.java
+++ Project01/JavaFXApplication2/src/javafxapplication2/JavaFXApplication2.java
@@ -10,7 +10,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -18,33 +18,38 @@ import javafx.stage.Stage;
  * @author Eshan
  */
 public class JavaFXApplication2 extends Application {
-    
-    @Override
-    public void start(Stage primaryStage) {
-        
-        Button btn = new Button("Build CV");
-        btn.setOnAction(new EventHandler<ActionEvent>(){
-            
-            @Override
-            public void handle(ActionEvent event){
-             System.out.println("Your CV is ready to print");    
-            }
-        
-    });
-        
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        Scene scene = new Scene(root, 500, 300);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         launch(args);
     }
-    
+       
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+        Button btn = new Button("Build CV");
+        Button exit = new Button("Exit");
+        
+        exit.setOnAction(e -> {
+            System.out.println("Thanks for using the system");
+            System.exit(0);  
+        });
+
+        btn.setOnAction(new EventHandler<ActionEvent>(){
+            
+            @Override
+            public void handle(ActionEvent event){
+             System.out.println("Your CV is ready to print");    
+            }   
+    });
+        
+        VBox root = new VBox();
+        root.getChildren().addAll(btn, exit);
+        Scene scene = new Scene(root, 800, 400);
+        primaryStage.setTitle("CV Builder");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
+ 
