8426e666856087a47d2ead2d6747fa8b4769b237
BaruaEshan <eshan.barua@northsouth.edu>
2019-08-15 02:52:54 +0600

    Giving inputs to the fields and Registrating successfully with Selenium Web Driver

--- Project02/Code/SeleniumWebTest/src/co/demosite/selenium/webdriver/webtest/ElementLocatorTest.java
+++ Project02/Code/SeleniumWebTest/src/co/demosite/selenium/webdriver/webtest/ElementLocatorTest.java
@@ -30,6 +30,55 @@ WebDriver driver;
 	
 	public void ElementLocator() {
 		
+		try {
+			invokeBrowser("http://newtours.demoaut.com/mercuryregister.php");
+			
+			driver.findElement(By.name("firstName")).sendKeys("Eshan");
+			Thread.sleep(2000);
+			
+			driver.findElement(By.name("lastName")).sendKeys("Barua");
+			Thread.sleep(2000);
+
+			driver.findElement(By.name("phone")).sendKeys("01883165981");
+			Thread.sleep(2000);
+
+			driver.findElement(By.name("userName")).sendKeys("Eshan");
+			Thread.sleep(2000);
+			
+			driver.findElement(By.name("address1")).sendKeys("125, Ahmedbag");
+			Thread.sleep(2000);
+
+			driver.findElement(By.name("address2")).sendKeys("Bashabo");
+			Thread.sleep(2000);
+
+			driver.findElement(By.name("city")).sendKeys("Dhaka");
+			Thread.sleep(2000);
+
+			driver.findElement(By.name("state")).sendKeys("Dhaka");
+			Thread.sleep(2000);
+
+			driver.findElement(By.name("postalCode")).sendKeys("1214");
+			Thread.sleep(2000);
+
+			driver.findElement(By.name("country")).sendKeys("BANGLADESH");
+			Thread.sleep(2000);
+			
+			driver.findElement(By.name("email")).sendKeys("Eshan");
+			Thread.sleep(2000);
+
+			driver.findElement(By.name("password")).sendKeys("1234");
+			Thread.sleep(2000);
+			
+			driver.findElement(By.name("confirmPassword")).sendKeys("1234");
+			Thread.sleep(2000);
+			
+			driver.findElement(By.name("register")).click();
+			Thread.sleep(2000);
+
+			driver.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
