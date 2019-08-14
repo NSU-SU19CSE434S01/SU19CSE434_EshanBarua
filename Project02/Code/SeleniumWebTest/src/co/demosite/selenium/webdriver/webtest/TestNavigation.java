f4758c34dd728f8f7ef6f8ec04cc33966cfd5b0c
BaruaEshan <eshan.barua@northsouth.edu>
2019-08-14 23:15:07 +0600

    Adding TestNavigation class to test navigation from one page to another page with Selenium Web driver

---
+++ Project02/Code/SeleniumWebTest/src/co/demosite/selenium/webdriver/webtest/TestNavigation.java
@@ -0,0 +1,49 @@
+package co.demosite.selenium.webdriver.webtest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestNavigation {
+	
+	WebDriver driver;
+	
+	public void invokeBrowser(){
+		
+		try {
+			System.setProperty("webdriver.chrome.driver", "D:\\Studyz\\CSE 434\\project\\Project02\\Code\\chromedriver_win32_2\\chromedriver.exe");
+			driver = new ChromeDriver();
+			
+			driver.manage().window().maximize();
+			driver.manage().deleteAllCookies();
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+			navigate();
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void navigate() {
+		
+		try {
+
+			
+		} catch (Exception e) { 
+			
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		TestNavigation myObj = new TestNavigation();
+		myObj.invokeBrowser();
+		
+	}
+
+}
