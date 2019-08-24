913f333e84949d44303b4930a6223f1238489ecb
BaruaEshan <eshan.barua@northsouth.edu>
2019-08-25 02:31:25 +0600

    adding TestNG in project

---
+++ Project02/Code/SeleniumWebTest/src/testNG/webTest.java
@@ -0,0 +1,22 @@
+package testNG;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class webTest {
+	
+  @BeforeTest
+  public void before() {
+	  
+  }
+  
+  @Test
+  public void f() {
+  }
+  
+  @AfterTest
+  public void after() {
+	  
+  }
+}
